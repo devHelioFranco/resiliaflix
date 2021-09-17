0643ddb59e01f17e7a55af79c828bed87ab45f80
unknown <israelmarques0001@gmail.com>
2021-09-16 22:21:18 -0300

    renomeando arquivos e  ajustando os links dos botões

---
+++ js/signup.js
@@ -0,0 +1,14 @@
+const cep=document.querySelector("#meuCep").value;
+
+cep.addEventListener("blur", function (e){
+console.log(cep.value)
+let opcoes{
+    method
+}
+fetch(`https://viacep.com.br/ws${cep}/json`)
+try{
+
+}catch{
+
+}
+})
