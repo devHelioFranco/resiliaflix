f5409612b28a5b3bfba2a9a3fc44462ad11a1b93
unknown <israelmarques0001@gmail.com>
2021-09-16 23:18:30 -0300

    commit teste marge

--- js/signup.js
+++ js/signup.js
@@ -2,13 +2,18 @@ const cep=document.querySelector("#meuCep").value;
 
 cep.addEventListener("blur", function (e){
 console.log(cep.value)
-let opcoes{
-    method
+let opcoes={
+    method:"GET",
+   mode:"cors",
+   cache:"default"
 }
-fetch(`https://viacep.com.br/ws${cep}/json`)
+
+fetch(`https://viacep.com.br/ws${cep}/json`, opcoes)
 try{
-
-}catch{
-
+then(response=>{ response.json()
+.then(data=>console.log(data))
+})
 }
+catch(erro=>console.log("Error: "+ erro,menssage))
+
 })
