72c46745164969a825bbccedf4a931abdbf4bafa
Helio Franco <dev.francohel@gmail.com>
2021-09-17 02:52:23 -0300

    fix: concertando problemas de responsividade na page signup

--- js/signup.js
+++ js/signup.js
@@ -1,19 +1,29 @@
-const cep=document.querySelector("#meuCep").value;
+// const cep=document.querySelector("#meuCep").value;
 
-cep.addEventListener("blur", function (e){
-console.log(cep.value)
-let opcoes={
-    method:"GET",
-   mode:"cors",
-   cache:"default"
-}
+// cep.addEventListener("blur", function (e){
+// console.log(cep.value)
+// let opcoes={
+//     method:"GET",
+//    mode:"cors",
+//    cache:"default"
+// }
 
-fetch(`https://viacep.com.br/ws${cep}/json`, opcoes)
-try{
-then(response=>{ response.json()
-.then(data=>console.log(data))
+// fetch(`https://viacep.com.br/ws${cep}/json`, opcoes)
+// try{
+// then(response=>{ response.json()
+// .then(data=>console.log(data))
+// })
+// }
+// catch(erro=>console.log("Error: "+ erro,menssage))
+
+// })
+
+let entrar = document.getElementById('entrar')
+entrar.addEventListener('click', function(){
+    return location = ('../index.html');
 })
-}
-catch(erro=>console.log("Error: "+ erro,menssage))
 
+let inscrever = document.getElementById('inscrever')
+inscrever.addEventListener('click', function(){
+    return location = ('./login.html');
 })
