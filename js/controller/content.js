24d35ac6f43a855dfaef462503bf1cc24140621d
Helio Franco <dev.francohel@gmail.com>
2021-09-18 22:37:39 -0300

    fix: ajustando ancoras e redirecionamento page info

--- js/controller/content.js
+++ js/controller/content.js
@@ -13,3 +13,17 @@ window.onload = () => {
     load();
 }
 
+
+let contato = document.getElementById('entrar')
+contato.addEventListener('click', ()=>{
+    console.log ('teste')
+    return location = ('./contact.html')
+})
+
+let sair = document.getElementById('inscrever')
+sair.addEventListener('click', ()=>{
+    console.log ('teste2')
+    return location = ('../index.html')
+})
+
+
