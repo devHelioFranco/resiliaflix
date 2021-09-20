5cd6bdb16eb4512a9bdfc898790b5718e4c96d9f
Helio Franco <dev.francohel@gmail.com>
2021-09-20 16:12:11 -0300

    feat: implementando requisição API pela pesquisa

--- js/controller/content.js
+++ js/controller/content.js
@@ -41,6 +41,15 @@ async function  apiFilme(filme){
     let url =  `https://www.omdbapi.com/?apikey=${key}&t=${filme}`
     let apiResponse = await fetch(url).then(response => response.json());
    console.log(apiResponse)
-
+   $('.list-content').css('display', 'none')
+   $('#infoFilme').css('display', 'flex')
+   $('#tituloFilme').html(`${apiResponse.Title}`)
+   $('#diretor').html(`Diretor: ${apiResponse.Director}`)
+   $('#ano').html(`Ano: ${apiResponse.Year}`)
+   $('#lancamento').html(`Lançamento: ${apiResponse.Release}`)
+   $('#duracao').html(`Duração: ${apiResponse.Runtime}`)
+   $('#historia').html(`Sinopse: ${apiResponse.Plot}`)
+   $('#poster').html(`<img src="${apiResponse.Poster}">`)
+  
 }
 
