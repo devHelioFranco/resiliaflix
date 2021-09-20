0d702df336a3b55b7e199090439ca5d54778ae6b
Helio Franco <dev.francohel@gmail.com>
2021-09-20 15:08:03 -0300

    feat: implementando api OMDB

--- js/controller/content.js
+++ js/controller/content.js
@@ -26,4 +26,21 @@ sair.addEventListener('click', ()=>{
     return location = ('../index.html')
 })
 
+let filme = ''
+// uso de APi
+var botao = document.getElementById('pesquisaBotao')
+botao.addEventListener('click' ,()=>{
+    filme = $('#pesquisa-input').val();
+    
+    apiFilme(filme)
+  
+})
+
+async function  apiFilme(filme){
+    const key = "c63395a9"
+    let url =  `https://www.omdbapi.com/?apikey=${key}&t=${filme}`
+    let apiResponse = await fetch(url).then(response => response.json());
+   console.log(apiResponse)
+
+}
 
